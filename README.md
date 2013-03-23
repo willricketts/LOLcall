3720cdc28d2de103dddb697ba1576693dc72183f
Will Ricketts <will@willricketts.com>
2013-03-23 13:58:05 -0400

    LOLcall. utterly ridiculous

---
+++ README.md
@@ -0,0 +1,4 @@
+Will Ricketts
+www.willricketts.com
+
+LOLcall is a simple ruby script that uses the Twilio API to send a TTS call to the target number.
---
+++ callthing.rb
@@ -0,0 +1,21 @@
+require 'twilio-ruby'
+
+puts "Who do you want to send this shit to?"
+number = gets.chomp
+
+puts "What do you want to say to them?"
+message = gets.chomp
+
+message.gsub!(/\s/,'+')
+
+receiver = number
+phone_number = '' # Twilio Account Phone Number
+account_sid = '' # TWILIO ACCT SID
+auth_token = '' # TWILIO AUTH TOKEN
+
+client = Twilio::REST::Client.new account_sid, auth_token
+client.account.calls.create(
+	:from => phone_number,
+	:to => receiver,
+	:url => 'http://twimlets.com/echo?Twiml=%3CResponse%3E%3CSay%3E' + message + '%3C%2FSay%3E%3C%2FResponse%3E'
+	)
